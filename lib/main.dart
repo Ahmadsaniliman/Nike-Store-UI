52732930318d2defb3ff9e32abfe5cb1e87452e3
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-01-18 21:41:01 +0100

    Amost done with ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:nike_store_ui/Constants/routes.dart';
+import 'package:nike_store_ui/Favourite/favourite.dart';
+import 'package:nike_store_ui/ForgotPassword/forgot_password.dart';
+import 'package:nike_store_ui/HomePage/home_page.dart';
 import 'package:nike_store_ui/Model/search.dart';
-import 'package:nike_store_ui/Profile/profile_data.dart';
+import 'package:nike_store_ui/OnBoardingScreens/Components/on_boarding_screen_2.dart';
+import 'package:nike_store_ui/OnBoardingScreens/Components/on_boarding_screen_3.dart';
+import 'package:nike_store_ui/OnBoardingScreens/on_borading_screen.dart';
+import 'package:nike_store_ui/Otp/otp_screen.dart';
+import 'package:nike_store_ui/Profile/profile.dart';
+import 'package:nike_store_ui/Register/register.dart';
+import 'package:nike_store_ui/SignIn/sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +26,21 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
       home: const SearchScreen(),
+      routes: {
+        homeScreenRoute: (context) => const HomeScreen(),
+        registerRoute: (context) => const RegisterScreen(),
+        loginRoute: (context) => const SignInScreen(),
+        forgotPasswordRoute: (context) => const ForgotPasswordScreen(),
+        optRoute: (context) => const OtperificationScreen(),
+        onBoardingScreenOneRoute: (context) => const OnBoardingScreen(),
+        // ignore: equal_keys_in_map
+        onBoardingScreenTwoRoute: (context) => const OnBoardingScreen2(),
+        // ignore: equal_keys_in_map
+        onBoardingScreenThreeRoute: (context) => const OnBoardingScreen3(),
+        searchRoute: (context) => const SearchScreen(),
+        favouriteRoute: (context) => const FavouriteScreen(),
+        profileRoute: (context) => const ProfileScreen(),
+      },
     );
   }
 }
